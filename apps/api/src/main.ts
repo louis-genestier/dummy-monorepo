b73bf22f7c4a2f640bd18bb93fbf7496eacb3a65
Louis Genestier <louis.genestier@gmail.com>
2023-06-22 08:56:07 +0200

    chore: trigger api actions

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,6 @@ import { app } from './app/app';
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-// Instantiate Fastify with some config
 const server = Fastify({
   logger: true,
 });
